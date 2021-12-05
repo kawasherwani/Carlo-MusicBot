6476e21032e224f06fc53db05a518661a3f29de4
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-05 23:18:56 +0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,7 +1,7 @@
 const createBar = require("string-progressbar");
 const { MessageEmbed } = require("discord.js");
 
-const { LOCALE } = require("../util/EvobotUtil");
+const { LOCALE } = require("../util/BotUtil");
 const i18n = require("i18n");
 
 i18n.setLocale(LOCALE);
