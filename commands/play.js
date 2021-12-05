47eadd6b3674e8dc291dada840f17e7fa5bb5e34
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-05 23:20:34 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,7 +4,7 @@ const YouTubeAPI = require("simple-youtube-api");
 const scdl = require("soundcloud-downloader").default;
 const https = require("https");
 const { MessageEmbed } = require("discord.js");
-const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, LOCALE, DEFAULT_VOLUME } = require("../util/EvobotUtil");
+const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, LOCALE, DEFAULT_VOLUME } = require("../util/BotUtil");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 const i18n = require("i18n");
 
